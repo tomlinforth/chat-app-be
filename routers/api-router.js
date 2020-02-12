e02a21879fb6e1623fbb5403a464ca57747d707c
carlos <carlos.beltran.exposito@gmail.com>
2020-02-12 10:53:39 +0000

    routing

--- routers/api-router.js
+++ routers/api-router.js
@@ -1,5 +1,13 @@
 const apiRouter = require("express").Router();
 
-apiRouter.use("/users", );
+apiRouter.use("/users");
+
+apiRouter.route("/users").get(getUsers);
+apiRouter.route("/users/:username").get(getUserByUserName);
+apiRouter.route("/channels").get(getChannels);
+apiRouter
+  .route("/channels/:channel_id/messages")
+  .get(getMessages)
+  .post(postMessage);
 
 module.exports = apiRouter;
