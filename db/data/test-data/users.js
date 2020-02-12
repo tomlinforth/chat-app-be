1f8479d59625862b7a074c01849d9745562d02f6
carlos <carlos.beltran.exposito@gmail.com>
2020-02-12 09:35:12 +0000

    test-data

---
+++ db/data/test-data/users.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    username: "user1",
+    password: "password",
+    name: "user1",
+    avatar_url:
+      "https://iupac.org/wp-content/uploads/2018/05/default-avatar.png"
+  },
+  {
+    username: "user2",
+    password: "password",
+    name: "user2",
+    avatar_url:
+      "https://iupac.org/wp-content/uploads/2018/05/default-avatar.png"
+  },
+  {
+    username: "user3",
+    password: "password",
+    name: "user3",
+    avatar_url:
+      "https://iupac.org/wp-content/uploads/2018/05/default-avatar.png"
+  }
+];
